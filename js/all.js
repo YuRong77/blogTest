4b389cd8f887d42512eb8f33cf3c0cec5882db08
Yu Rong <gcobc50335@gmail.com>
2021-03-31 22:44:18 +0800

    add 新增 刪除

--- js/all.js
+++ js/all.js
@@ -1,12 +1,49 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
+  el: "#app",
+  data: {
+    list: [],
+    newItem: "",
+  },
+  methods: {
+    getList() {
+      axios
+        .get("https://node-crud-777.herokuapp.com/user/getList")
+        .then((res) => {
+          this.list = res.data.Result;
+        })
+        .catch((err) => {
+          console.log("XHR錯誤");
+        });
     },
-    methods: {
-
+    addItem() {
+      const data = {
+        content: this.newItem,
+      };
+      axios
+        .post("https://node-crud-777.herokuapp.com/user/addList", data)
+        .then((res) => {
+          this.newItem = "";
+          this.getList();
+        })
+        .catch((err) => {
+          console.log("XHR錯誤");
+        });
     },
-    created() {
-        
-    }
-  })
+    removeItem(_id) {
+      const data = {
+        id: _id,
+      };
+      axios
+        .post("https://node-crud-777.herokuapp.com/user/deleteList", data)
+        .then((res) => {
+          this.getList();
+        })
+        .catch((err) => {
+          console.log("XHR錯誤");
+        });
+    },
+  },
+  created() {
+    this.getList();
+  },
+});
